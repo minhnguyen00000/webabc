5ec9c65d76f4bca5fef5bc0379968047ab234462
nguyen00000 <minhnguyen020699@gmail.com>
2020-12-30 11:30:50 +0700

    up chuc nang khach hang

--- index.php
+++ index.php
@@ -113,9 +113,9 @@ $so1trang=8;
 			<ul>
 				<li><a href="index.php">Trang Chủ</a></li>
 				<li><a href="index.php?ac=dienthoai">Điện Thoại</a></li>
-				<li><a href="#">Đăng Ký</a></li>
+				<li><a href="index.php?ac=dangky">Đăng Ký</a></li>
 				<li><a href="#">Giỏ hàng</a></li>
-				<li><a href="#">Thông tin khách hàng</a></li>
+				<li><a href="index.php?ac=thongtinkh">Thông tin khách hàng</a></li>
 				
 				<div class="clear"></div>
 			</ul>
